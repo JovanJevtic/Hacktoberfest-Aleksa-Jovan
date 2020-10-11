f1190b2cefb6662521bd078710a623df12291075
Jovan Jevtic <jovanjevtic132@gmail.com>
2020-10-11 22:31:48 +0200

    Heroku

--- serverSide/server.js
+++ serverSide/server.js
@@ -20,6 +20,14 @@ dotenv.config({
 // Routing
 app.use('/', router);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+  })
+}
+
 // Port listening
 const PORT = process.env.PORT || 5000;
 
