c1c33e33a7d1b670b6fc1fec6b47fd66dbc747fd
Jovan Jevtic <jovanjevtic132@gmail.com>
2020-10-12 20:14:27 +0200

    Build folder

--- serverSide/server.js
+++ serverSide/server.js
@@ -26,10 +26,10 @@ app.use('/api/posts', postsRouter);
 
 //? Static files serve
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.use(express.static(path.join(__dirname, '../client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
